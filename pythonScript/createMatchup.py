32473b7d6fed4dbd33fe9d687b033af54744f992
Kevin Durrenberger <kevindurrenberger@gmail.com>
2018-10-03 14:02:52 -0400

    Fixed offset number issue

--- pythonScript/createMatchup.py
+++ pythonScript/createMatchup.py
@@ -3,8 +3,8 @@ import requests
 import json
 import random
 
-#url = 'https://comicvine.gamespot.com/api/publisher/4010-31/?api_key=afbe58eef9e2d34188a29ebf707d671bde76e1a1'
-#headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36'}
+url = 'https://comicvine.gamespot.com/api/publisher/4010-31/?api_key=afbe58eef9e2d34188a29ebf707d671bde76e1a1'
+headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36'}
 #result = requests.get(url, headers=headers)
 #print(result.status_code)
 #print(result.text)
@@ -16,13 +16,15 @@ with open("MarvelJSON.json","r") as read_file:
 
 print("Here are 5 DC characters")
 for x in range(5):
-    dcCharacterName = data["results"]["characters"][random.randint(1,3000)]["name"]
-    dcCharacterURL = data["results"]["characters"][random.randint(1,3000)]["api_detail_url"]
+    offset= random.randint(1,3000)
+    dcCharacterName = data["results"]["characters"][offset]["name"]
+    dcCharacterURL = data["results"]["characters"][offset]["api_detail_url"]
     print(dcCharacterName)
 print("Here are 5 Marvel characters")
 for x in range(5):
-    marvelCharacterName=mData["results"]["characters"][random.randint(1,3000)]["name"]
-    marvelCharacterURL= mData["results"]["characters"][random.randint(1,3000)]["api_detail_url"] 
+    offset= random.randint(1,3000)
+    marvelCharacterName=mData["results"]["characters"][offset]["name"]
+    marvelCharacterURL= mData["results"]["characters"][offset]["api_detail_url"] 
     print (marvelCharacterName)
      
 print("Who Would Win")
