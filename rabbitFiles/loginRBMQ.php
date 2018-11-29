8ac2d365b6260ffe7823e697be7ea9f59cdb30ca
Kevin Durrenberger <kevindurrenberger@gmail.com>
2018-11-29 00:36:41 -0500

    front end attempts to work with backend failover

--- rabbitFiles/loginRBMQ.php
+++ rabbitFiles/loginRBMQ.php
@@ -1,27 +1,42 @@
 <?php
+
 require_once('path.inc');
 require_once('get_host_info.inc');
 require_once('rabbitMQLib.inc');
 
 function login($username,$pass)
 {
-	$client = new rabbitMQClient("testRabbitMQ.ini","testServer");
-	#$bClient = new rabbitMQClient("testRabbitMQ.ini","backupServer");
-	$request1 = array();
-	$request1['type'] = "login";
-        $request1['username'] = $username;
-	$request1['password'] = $pass;
-	$response = $client->send_request($request1);
-	if ($response == 69)
-        {
-                $bClient = new rabbitMQClient("testRabbitMQ.ini","backupServer");
-                $response= $bClient->send_request($request1);
-        }
+	if (!isset($_SESSION['backup']))
+	{
+	
+		$client = new rabbitMQClient("testRabbitMQ.ini","testServer");
+		#$bClient = new rabbitMQClient("testRabbitMQ.ini","backupServer");
+		$request1 = array();
+		$request1['type'] = "login";
+       		$request1['username'] = $username;
+		$request1['password'] = $pass;
+		$response = $client->send_request($request1);
+		if ($response == 69)
+		{
+			$_SESSION['backup'] = true;
+	                $bClient = new rabbitMQClient("backupMQ.ini","testServer");
+	                $response= $bClient->send_request($request1);
+		}
+	}
+	else
+	{
+		$bClient= new rabbitMQClient("backupMQ.ini", "testServer");
+		$response = $bClient->send_request($request1);
+	}
+
 
 	return $response;
 }
 function register($userN,$email, $pass,$firstN,$lastN)
 {
+	if (!isset($_SESSION['backup']))
+        {
+
 	$client = new rabbitMQClient("testRabbitMQ.ini","testServer");
 	#$bClient = new rabbitMQClient("testRabbitMQ.ini","backupServer");
 	$request2 = array();
@@ -33,16 +48,27 @@ function register($userN,$email, $pass,$firstN,$lastN)
 	$request2['lastName'] = $lastN;
 	$response = $client->send_request($request2);
 	if ($response == 69)
-        {
-                $bClient = new rabbitMQClient("testRabbitMQ.ini","backupServer");
+	{
+		$_SESSION['backup'] = true;
+                $bClient = new rabbitMQClient("backupMQ.ini","testServer");
                 $response= $bClient->send_request($request2);
+	}
+	}
+	else
+        {
+                $bClient= new rabbitMQClient("backupMQ.ini", "testServer");
+                $response = $bClient->send_request($request2);
         }
+
 
 	return $response;
 }
 function errorThrow($msg)
 {
-	$eClient = new rabbitMQClient("testRabbitMQ.ini","errorServer");
+	if (!isset($_SESSION['backup']))
+        {
+
+	$eClient = new rabbitMQClient("testRabbitMQ.ini","testServer");
 	$request3= array();
 	$eDate = time();
 
@@ -52,61 +78,109 @@ function errorThrow($msg)
 	$request3['msg'] =$msg;
 	$eClient->send_request($request3);
 	if ($response == 69)
-        {
-                $bClient = new rabbitMQClient("testRabbitMQ.ini","backupServer");
+	{
+		$_SESSION['backup'] = true;
+                $bClient = new rabbitMQClient("backupMQ.ini","testServer");
                 $response= $bClient->send_request($request3);
+	}
+	}
+	else
+        {
+                $bClient= new rabbitMQClient("backupMQ.ini", "testServer");
+                $response = $bClient->send_request($request3);
         }
+
 
 }
 function validateSession($userName,$sessionID)
 {
+	if (!isset($_SESSION['backup']))
+        {
+
 	$client = new rabbitMQClient("testRabbitMQ.ini","testServer");
 	#$bClient = new rabbitMQClient("testRabbitMQ.ini","backupServer");
 	$request4= array();
 	$request4['type']="validate";
 	$request4['username']= $userName;
 	$request4['sessionID']= $sessionID;
-	$response= $client->send_request($request4);
+	$response= $client->send_request($request4);	
 	if ($response == 69)
 	{
-		$bClient = new rabbitMQClient("testRabbitMQ.ini","backupServer");
+		$_SESSION['backup'] = true;
+		$bClient = new rabbitMQClient("backupMQ.ini","testServer");
 		$response= $bClient->send_request($request4);
 	}
+	}
+	else
+        {
+                $bClient= new rabbitMQClient("backupMQ.ini", "testServer");
+		$response = $bClient->send_request($request4);
+		if ($response == 69)
+		{
+			return false;
+		}
+        }
+
 	return $response;
 }
 function getDaily()
 {
+	if (!isset($_SESSION['backup']))
+        {
+
 	$client = new rabbitMQClient("testRabbitMQ.ini","testServer");
 	#$bClient = new rabbitMQClient("testRabbitMQ.ini","backupServer");
 	$request5 = array();
 	$request5['type']="dailyMatchup";
 	$response= $client->send_request($request5);
 	if ($response == 69)
-        {
-                $bClient = new rabbitMQClient("testRabbitMQ.ini","backupServer");
+	{
+		$_SESSION['backup'] = true;
+                $bClient = new rabbitMQClient("backupMQ.ini","testServer");
                 $response= $bClient->send_request($request5);
+	}
+	}
+	else
+        {
+                $bClient= new rabbitMQClient("backupMQ.ini", "testServer");
+                $response = $bClient->send_request($request5);
         }
+
 
 	return $response;
 }
 function getLB()
 {
-	$client = new rabbitMQClient("testRabbitMQ.ini","testServer");
+	if (!isset($_SESSION['backup']))
+        {
+
+	$client = new rabbitMQClient("backupMQ.ini","testServer");
 	#$bClient = new rabbitMQClient("testRabbitMQ.ini","backupServer");
 	$request7 = array();
 	$request7['type']= "leaderboard";
 	$response = $client->send_request($request7);
 	if ($response == 69)
-        {
-                $bClient = new rabbitMQClient("testRabbitMQ.ini","backupServer");
+	{
+		$_SESSION['backup'] = true;
+                $bClient = new rabbitMQClient("backupMQ.ini","testServer");
                 $response= $bClient->send_request($request7);
+	}
+	}
+	else
+        {
+                $bClient= new rabbitMQClient("backupMQ.ini", "testServer");
+                $response = $bClient->send_request($request7);
         }
+
 
 	return $response;
 }
 function vote($userName, $vote)
 {
-	$client = new rabbitMQClient("testRabbitMQ.ini", "testServer");
+	if (!isset($_SESSION['backup']))
+        {
+
+	$client = new rabbitMQClient("backupMQ.ini", "testServer");
 	#$bClient = new rabbitMQClient("testRabbitMQ.ini","backupServer");
 	$request6= array();
 	$request6['type']="vote";
@@ -114,15 +188,26 @@ function vote($userName, $vote)
 	$request6['vote']=$vote; 
 	$response=$client->send_request($request6);
 	if ($response == 69)
-        {
-                $bClient = new rabbitMQClient("testRabbitMQ.ini","backupServer");
+	{
+		$_SESSION['backup'] = true;
+                $bClient = new rabbitMQClient("backupMQ.ini","testServer");
                 $response= $bClient->send_request($request6);
+	}
+	}
+	else
+        {
+                $bClient= new rabbitMQClient("backupMQ.ini", "testServer");
+                $response = $bClient->send_request($request6);
         }
+
 	return $response;
 
 }
 function giveTokens($userName)
 {
+	if (!isset($_SESSION['backup']))
+        {
+
 	$client = new rabbitMQClient("testRabbitMQ.ini", "testServer");
 	#$bClient = new rabbitMQClient("testRabbitMQ.ini","backupServer");
 	$request= array();
@@ -130,10 +215,18 @@ function giveTokens($userName)
 	$request['username']= $userName;
 	$response = $client->send_request($request);
 	if ($response == 69)
-        {
-                $bClient = new rabbitMQClient("testRabbitMQ.ini","backupServer");
+	{
+		$_SESSION['backup'] = true;
+                $bClient = new rabbitMQClient("backupMQ.ini","testServer");
                 $response= $bClient->send_request($request);
+	}
+	}
+	else
+        {
+                $bClient= new rabbitMQClient("backupMQ.ini", "testServer");
+                $response = $bClient->send_request($request);
         }
+
 	return $response;
 }
 
