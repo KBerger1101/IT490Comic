e7f82e41b578a5d95e36e6ea5678ca7c85e61c72
Kevin Durrenberger <kevindurrenberger@gmail.com>
2018-12-17 12:02:05 -0500

    added heartbeat for failover

---
+++ backend/heartbeatdaily.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+mainIP=10.128.82.112
+
+while true;
+do
+	ping -c 1 $mainIP > /dev/null 2>&1
+	if [ $? -ne 0 ]
+	then
+	       	echo "Main server down"
+		php /home/kevin/git/IT490Comic/backend/dailyWinnerCalc.php
+
+	fi
+
+done
