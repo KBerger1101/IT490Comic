640804f32ab012a484c3a75c0e254e16c45ce938
Jake <jgp25@njit.edu>
2018-12-17 19:48:22 -0500

    Finalized Tutorial Page

--- loginFiles/tutorial.php
+++ loginFiles/tutorial.php
@@ -1,5 +1,5 @@
 <html>
-<h1>Tutorial</h1>
+
 	<head>
 
     <meta charset="utf-8">
@@ -27,8 +27,37 @@
   </div>
 </nav>
 
-<script src="js/jquery-3.2.1.min.js" type="text/javascript"></script>
-<script src="js/popper.min.js" type="text/javascript"></script>
+<h1>Tutorial</h1>
+<br>
+Welcome to <b><i>EPIC Hero Challenges</i></b> where your favorite super heros will form teams to determine who will be the winner! This page will provide a breif overview of what you can do on the site.
+<br><br>
+Users will begin be Registering, or signing in if they have already created a user account. You cannot use the site without an account as your 'tokens' are applied directly to your account upon joining.
+<br><br>
+Once you've joined/signed into the site, you'll be directed to the main menu. These options are also available on the navigation bar. Here, you can;
+<i>
+<ol>
+	<li>View Daily Matchup</li>
+    <li>View the Leaderboards</li>
+    <li>Purchase Additional Tokens</li>
+    <li>View this tutorial page</li>
+    <li>Log out of the site.</li>
+</ol></i>
+<p><br>
+  <br>
+  To vote on your favorite team; click <i><strong>View Daily Matchup.</strong></i><br>
+  <img src="choicePage.PNG" width="299" height="321"> </p>
+<p><br>
+  Select the team you would like to vote for, and click <b>"Submit Vote"</b><br>
+  <img src="dailyPage.PNG" width="672" height="536"><br>
+  When you return to the Main Menu, You'll see that your token count has lost '100' as you've placed a 'bet' on the team. You can bet multiple times in the same day without loosing additional tokens. To add additional token to your account, visit the <i>Buy Now</i> and purchase additional token through PayPal. View your standing on the leaderboard by selecting <em><strong>&quot;View Leaderboards.&quot;</strong></em><br>
+</p>
+<p><img src="leaderboard.PNG" width="203" height="266"></p>
+<p><br>
+  If you are chosen as the weekly winner, the tokens that will be deposited into your account will be of the total tokens divided by the total number of winners. You can use those tokens are you normally would to vote on the team you believe will win.<br><br>
+  <b><i>Winning teams are chosen weekly.</i></b>
+  <script src="js/jquery-3.2.1.min.js" type="text/javascript"></script>
+  <script src="js/popper.min.js" type="text/javascript"></script>
+</p>
 </body>
 </html>
 
